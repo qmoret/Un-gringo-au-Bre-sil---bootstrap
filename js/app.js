9dc7aa4d29ebfd89679cb31ceaae3d94622a8d23
qmoret <qmoretdero@gmail.com>
2015-07-07 14:55:20 -0400

    Added text & dates, cleaned HTML & js
    Added some text, reduced size, added dates (h5), added slide bh.
    cleaned app.js

--- js/app.js
+++ js/app.js
@@ -5,25 +5,13 @@ var main = function () {
     // Prevent the jump and the #hash from appearing on the address bar
         e.preventDefault();
     // Scroll the window, stop any previous animation, stop on user manual scroll
-        $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:true});
+        $(window).stop(true).scrollTo(this.hash, {duration:800, interrupt:true, easing : 'swing'});
     });
     $('a[href="#test"]').click(function() {
         $.scrollTo({target : "100px"});
     });
 };
-$(document).ready(function(){
-    $window = $(window);
-    //Captura cada elemento section com o data-type "background"
-    $('section[data-type="background"]').each(function(){
-        var $scroll = $(this);   
-            //Captura o evento scroll do navegador e modifica o backgroundPosition de acordo com seu deslocamento.            
-            $(window).scroll(function() {
-                var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-                var coords = '50% '+ yPos + 'px';
-                $scroll.css({ backgroundPosition: coords });    
-            });
-   });  
-});
+
 $(document).ready(main);
 $(document).ready($('.carousel').carousel(
 ));
