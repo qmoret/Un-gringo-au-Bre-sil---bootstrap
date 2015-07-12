5e942707788cefa42516b4a7028d54266edb5a1e
qmoret <qmoretdero@gmail.com>
2015-07-12 14:54:55 -0400

    Pleins de changements en tous genres

--- js/app.js
+++ js/app.js
@@ -17,3 +17,18 @@ $(document).ready($('.carousel').carousel(
 ));
 
 
+document.onscroll=function(){
+    //var height = $(window).height()
+bg=document.getElementById("bgsp");
+var scroll = window.pageYOffset;
+var off = bg.offsetTop;
+var entryinviewport = bg.offsetTop - $(window).height();
+var exitviewport = entryinviewport+2*$(window).height();
+if (scroll >=  entryinviewport && scroll<= exitviewport) {
+    var y = parseInt(bg.style.backgroundPositionY);
+    var adefile = scroll - entryinviewport;
+    var num = (y-adefile)*0.1+'px'
+    //$("#bgsp").animate({'background-position-y' : num });
+    bg.style.backgroundPositionY = num
+}
+}
